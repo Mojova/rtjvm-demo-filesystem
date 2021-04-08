a28ebe72becd5fbecb7981ae223fe3392903202e
Jaakko Kaisanlahti <jaakko.kaisanlahti@erpg.fi>
2021-04-08 10:42:22 +0300

    Lesson 41.

--- src/com/rtjvm/scala/oop/commands/Mkdir.scala
+++ src/com/rtjvm/scala/oop/commands/Mkdir.scala
@@ -1,6 +1,6 @@
 package com.rtjvm.scala.oop.commands
 
-import com.rtjvm.scala.oop.files.Directory
+import com.rtjvm.scala.oop.files.{DirEntry, Directory}
 import com.rtjvm.scala.oop.filesystem.State
 
 class Mkdir(name: String) extends Command {
@@ -9,7 +9,30 @@ class Mkdir(name: String) extends Command {
     name.contains(".")
   }
 
-  def doMkdir(state: State, name: String): State = ???
+  def updateStructure(currentDirectory: Directory, path: List[String], newEntry: DirEntry): Directory = {
+    if (path.isEmpty) currentDirectory.addEntry(newEntry)
+    else {
+      val oldEntry = currentDirectory.findEntry(path.head).asDirectory
+      currentDirectory.replaceEntry(oldEntry.name, updateStructure(oldEntry, path.tail, newEntry))
+    }
+  }
+
+  def doMkdir(state: State, name: String): State = {
+    val wd = state.wd
+    // 1. all the directories in the full path
+    val allDirsInPath = wd.getAllDirectoriesInPath()
+
+    // 2. create new directory entry in the working directory
+    val newDir = Directory.empty(wd.path, name)
+
+    // 3. update the whole directory struture starting from the root
+    val newRoot: Directory = updateStructure(state.root, allDirsInPath, newDir)
+
+    // 4. find new working directory instance given wd’s full path in the new directory structure
+    val newWd: Directory = newRoot.findDescendant(allDirsInPath)
+
+    State(newRoot, newWd)
+  }
 
   override def apply(state: State): State = {
     val wd = state.wd
--- src/com/rtjvm/scala/oop/files/Directory.scala
+++ src/com/rtjvm/scala/oop/files/Directory.scala
@@ -1,7 +1,19 @@
 package com.rtjvm.scala.oop.files
 
 class Directory(override val parentPath: String, override val name: String, val contents: List[DirEntry]) extends DirEntry(parentPath, name) {
+  def replaceEntry(entryName: String, newEntry: Directory): Directory = ???
+
+  def findEntry(entryName: String): DirEntry = ???
+
+  def addEntry(newEntry: DirEntry): Directory = ???
+
+  def findDescendant(allDirsInPath: List[String]): Directory = ???
+
+  def getAllDirectoriesInPath(): List[String] = path.substring(1).split(Directory.SEPARATOR).toList
+
   def hasEntry(entry: String): Boolean = ???
+
+  override def asDirectory: Directory = this
 }
 
 object Directory {
